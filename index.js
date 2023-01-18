61bc6328592607fc56d871b028e4c2d6c6ab4c0f
Ibrokhim <mr017programmer@gmail.com>
2023-01-18 14:38:12 +0500

    lesson-2

---
+++ index.js
@@ -0,0 +1,40 @@
+"use strict";
+// TypeScriptda asosiy ma'lumot turlari
+// let a = 12
+// a = "salom"
+// a = 1 + 2
+// a = 1 + "salom" // "1salom"
+let a;
+a = 17;
+// let b = "ibrohim";
+// b = 12
+// b = true
+// b = b + " Ismoilov";
+let b = "Ibrokhim";
+// let c = true;
+// c = 12
+// c = "salom"
+// c = false
+let c;
+c = false;
+// let d = null
+let d = null;
+// d = true
+let e = undefined;
+// let f = {}
+// let f: object = {}
+// f = 1
+// f = {name: "Ibrokhim"}
+// f.
+// let f: { name: string } = { name: "Ismoil" };
+const f = { lastName: "Ismoil" };
+// f = {lastName: "Ibrokhim"}
+f.lastName = "Ibrokhim";
+let g;
+g = 1;
+g = undefined;
+g = "string";
+g = false;
+g = function () { };
+g = {};
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,44 @@
+// TypeScriptda asosiy ma'lumot turlari
+// let a = 12
+// a = "salom"
+// a = 1 + 2
+// a = 1 + "salom" // "1salom"
+let a: number;
+a = 17;
+
+// let b = "ibrohim";
+// b = 12
+// b = true
+// b = b + " Ismoilov";
+let b: string = "Ibrokhim";
+
+// let c = true;
+// c = 12
+// c = "salom"
+// c = false
+let c: boolean;
+c = false;
+
+// let d = null
+let d: null = null;
+// d = true
+
+let e: undefined = undefined;
+
+// let f = {}
+// let f: object = {}
+// f = 1
+// f = {name: "Ibrokhim"}
+// f.
+// let f: { name: string } = { name: "Ismoil" };
+const f: { lastName: string } = { lastName: "Ismoil" };
+// f = {lastName: "Ibrokhim"}
+f.lastName = "Ibrokhim";
+
+let g: any;
+g = 1;
+g = undefined;
+g = "string";
+g = false;
+g = function () {};
+g = {};
