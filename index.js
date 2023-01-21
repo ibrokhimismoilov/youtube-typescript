61beb26a33aac9b3c7cf417664043648a93183b6
Ibrokhim <mr017programmer@gmail.com>
2023-01-22 00:11:44 +0500

    lesson-06 classes

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
 "use strict";
-// Massivlar, kortejlar va qayta hisoblash
-// let a = [];
-// a = [12, true, "str", []]
-// let a = [1, 2, 4]
-let a = [];
-a = [1];
-// a = [1, 2, true]
-let b = ["text", "str", ""];
-let c = [1, 2, 3];
-let d = ["", 12, ""];
-let e = [1, 2, 3, ""];
-// ===================================================
-// kortejlar
-let f = [1, "asd"];
-// f = [true]
-// f = [""]
-f = [1, "str"];
-let g;
-g = [12, "str", true];
-// let h: [number, string?, boolean]
-// ===================================================
-// Qayta hisoblash [Enums]
-var Gender;
-(function (Gender) {
-    Gender[Gender["Male"] = 10] = "Male";
-    Gender[Gender["Female"] = 11] = "Female";
-})(Gender || (Gender = {}));
-// console.log(Gender.Male, Gender.Female);
-// console.log("Male=" + Gender[Gender.Male], "Female" + Gender[Gender.Female]);
-var G;
-(function (G) {
-    G["A"] = "Hello";
-    G["B"] = "TypeScript";
-})(G || (G = {}));
-console.log("Hello" /* H.A */, "TypeScript" /* H.B */);
+// TypeScriptda sinflar
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
+    // 1ta klassda constructordan faqat 1 marotaba foydalaniladi
+    // constructor () {}
+    // method
+    info(value) {
+        return `${this._name} - ${this._age + value} yoshda`;
+    }
+    static description() {
+        return `Bu klass Person haqida......`;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
+    get age() {
+        return this._age;
+    }
+    set age(value) {
+        if (value > 0 && value < 100) {
+            this._age = value;
+        }
+        else {
+            throw new Error("Siz notog'ri yosh kiritidingiz");
+        }
+    }
+}
+// method1() {}
+// method2() {}
+Person.isFlaying = false;
+const ibrokhim = new Person("Ibrokhim", 23);
+const ismoil = new Person("Ismoil", 24);
+// const ibrokhim: { name: string; age: number } = { name: "Ibrokhim", age: 23 };
+// const ismoil: { name: string; age: number } = { name: "Ibrokhim", age: 23 };
+// console.log(ibrokhim.info(2));
+// console.log(Person.description());
+// console.log(ismoil);
+// console.log(ibrokhim);
+// ibrokhim.age = 24;
+// ibrokhim.age = -1;
+// console.log(ibrokhim.age);
+if ({} instanceof Person) {
+    console.log("Tegishli");
+}
+else {
+    console.log("Tegishl emas");
+}
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,55 +1,70 @@
-// Massivlar, kortejlar va qayta hisoblash
+// TypeScriptda sinflar
 
-// let a = [];
-// a = [12, true, "str", []]
-// let a = [1, 2, 4]
-let a: number[] = [];
-a = [1];
-// a = [1, 2, true]
+class Person {
+  _name: string;
+  _age: number;
 
-let b: string[] = ["text", "str", ""];
+  constructor(name: string, age: number) {
+    this._name = name;
+    this._age = age;
+  }
 
-let c: Array<number> = [1, 2, 3];
+  // 1ta klassda constructordan faqat 1 marotaba foydalaniladi
+  // constructor () {}
 
-let d: (number | string)[] = ["", 12, ""];
+  // method
+  info(value: number): string {
+    return `${this._name} - ${this._age + value} yoshda`;
+  }
+  // method1() {}
+  // method2() {}
 
-let e: Array<number | string> = [1, 2, 3, ""];
+  static isFlaying = false;
+  static description(): string {
+    return `Bu klass Person haqida......`;
+  }
 
-// ===================================================
-// kortejlar
+  get name(): string {
+    return this._name;
+  }
 
-let f: [number, string] = [1, "asd"];
-// f = [true]
-// f = [""]
-f = [1, "str"];
+  set name(value: string) {
+    this._name = value;
+  }
 
-let g: [number, string?, boolean?];
+  get age(): number {
+    return this._age;
+  }
 
-g = [12, "str", true];
-
-// let h: [number, string?, boolean]
-
-// ===================================================
-// Qayta hisoblash [Enums]
-
-enum Gender {
-  Male = 10,
-  Female,
+  set age(value: number) {
+    if (value > 0 && value < 100) {
+      this._age = value;
+    } else {
+      throw new Error("Siz notog'ri yosh kiritidingiz");
+    }
+  }
 }
 
-// console.log(Gender.Male, Gender.Female);
-// console.log("Male=" + Gender[Gender.Male], "Female" + Gender[Gender.Female]);
+const ibrokhim: Person = new Person("Ibrokhim", 23);
+const ismoil: Person = new Person("Ismoil", 24);
 
-enum G {
-  A = "Hello",
-  B = "TypeScript",
+// const ibrokhim: { name: string; age: number } = { name: "Ibrokhim", age: 23 };
+// const ismoil: { name: string; age: number } = { name: "Ibrokhim", age: 23 };
+
+// console.log(ibrokhim.info(2));
+
+// console.log(Person.description());
+// console.log(ismoil);
+
+// console.log(ibrokhim);
+
+// ibrokhim.age = 24;
+// ibrokhim.age = -1;
+
+// console.log(ibrokhim.age);
+
+if ({} instanceof Person) {
+  console.log("Tegishli");
+} else {
+  console.log("Tegishl emas");
 }
-
-// console.log(G.A, G.B, G[G.A]);
-
-const enum H {
-  A = "Hello",
-  B = "TypeScript",
-}
-
-console.log(H.A, H.B);
