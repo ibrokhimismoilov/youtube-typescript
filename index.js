8ccd1e76a4b9ecfc4ce30e6bcf119f4852cc3222
Ibrokhim <mr017programmer@gmail.com>
2023-01-26 21:34:23 +0500

    lesson-20 Utilits-5 Uppercase, Lowercase, Capitalize, UnCapitalize

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 "use strict";
 // TypeScript - usefull global utility types
-// Parameters, ConstructorParameters, ReturnType, InstanceType
-function function1(x, y) {
-    return x.length > y.toString().length;
-}
-// ======================================================================
-// ConstructorParameters
-class Class1 {
-    constructor(a, b) {
-        this.a = a;
-        this.b = b;
-    }
-}
+// Uppercase, Lowercase, Capitalize, UnCapitalize
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,67 +1,12 @@
 // TypeScript - usefull global utility types
-// Parameters, ConstructorParameters, ReturnType, InstanceType
+// Uppercase, Lowercase, Capitalize, UnCapitalize
 
-function function1(x: string, y: number): boolean {
-  return x.length > y.toString().length;
-}
+type SIZES = "sm" | "md" | "lg";
 
-type A = typeof function1;
+type U = Uppercase<SIZES>; // "SM" | "MD" | "LG"
 
-// Parameters
-type B = Parameters<A>; // [ string, number]
+type L = Lowercase<U>; // "sm" | "md" | "lg"
 
-type MyParametrs<T extends (...args: any) => any> = T extends (
-  ...args: infer U
-) => any
-  ? U
-  : never;
+type C = Capitalize<L>; // "Sm" | "Md" | "Lg"
 
-type B2 = MyParametrs<A>; // [string, number]
-
-// ======================================================================
-// ConstructorParameters
-
-class Class1 {
-  a: number;
-  b: string;
-
-  constructor(a: number, b: string) {
-    this.a = a;
-    this.b = b;
-  }
-}
-
-type C = typeof Class1;
-
-type D = ConstructorParameters<C>; // [a: number, b: string]
-
-type MyConstructorParameters<T extends new (...args: any) => any> =
-  T extends new (...args: infer U) => any ? U : never;
-
-type D2 = MyConstructorParameters<C>;
-
-// ======================================================================
-// ReturnType
-
-type E = ReturnType<A>;
-
-type MyReturnType<T extends (...args: any) => any> = T extends (
-  ...args: any
-) => infer U
-  ? U
-  : never;
-
-type E2 = MyReturnType<A>;
-
-// ======================================================================
-// InstanceType
-
-type F = InstanceType<C>; // Class1
-
-type MyInstanceType<T extends new (...args: any) => any> = T extends new (
-  ...args: any
-) => infer U
-  ? U
-  : never;
-
-type F2 = MyInstanceType<C>; // Class1
+type UC = Uncapitalize<U>; // "sM" | "mD" | "lG"
